c355908a18a9bfacf74cad905702272b951e9673
Marcus Lull <marcuslull@gmail.com>
2023-10-10 11:53:02 -0700

    Requirements Part A and B A - Create new project B - Create GitLab subgroup Initial commit

---
+++ src/main/java/com/example/d288pa/D288PaApplication.java
@@ -0,0 +1,13 @@
+package com.example.d288pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D288PaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D288PaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/d288pa/D288PaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.d288pa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D288PaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
